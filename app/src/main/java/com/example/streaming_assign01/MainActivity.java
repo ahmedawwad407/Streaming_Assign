3167e17da1d63c27f5e755bde3fe3c2cd9add9ad
AhmedAwwad <awwadahmed407@gmail.com>
2021-06-06 11:58:06 +0300

    welcome

---
+++ app/src/main/java/com/example/streaming_assign01/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.streaming_assign01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+    PlayerView playerView;
+    SimpleExoPlayer player;
+
+    String videoURl = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4";
+
+    private Boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playPackPosition = 0;
+
+    Button btnVideo_1;
+    Button goToActivity2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        playerView = findViewById(R.id.video_view);
+        btnVideo_1 = findViewById(R.id.Btn_Video1);
+        goToActivity2 = findViewById(R.id.goToActivity2);
+
+        btnVideo_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initVideo();
+            }
+        });
+        goToActivity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(i);
+                releaseVideo();
+            }
+        });
+    }
+
+
+    public void initVideo() {
+        //create player
+        player = ExoPlayerFactory.newSimpleInstance(this);
+        //connect Player with playerView
+        playerView.setPlayer(player);
+        //media source
+        Uri uri = Uri.parse(videoURl);
+        DataSource.Factory factory = new DefaultDataSourceFactory(this, "exoplayer-codelab");
+        MediaSource mediaSource = new ProgressiveMediaSource.Factory(factory).createMediaSource(uri);
+
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playPackPosition);
+        player.prepare(mediaSource, false, false);
+    }
+
+
+    public void releaseVideo() {
+        if (player != null) {
+            playWhenReady = player.getPlayWhenReady();
+            playPackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            player.release();
+            player = null;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (player != null) {
+            initVideo();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseVideo();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseVideo();
+    }
+}
---
+++ app/src/main/java/com/example/streaming_assign01/MainActivity2.java
@@ -0,0 +1,101 @@
+package com.example.streaming_assign01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+
+public class MainActivity2 extends AppCompatActivity {
+    PlayerView playerView;
+    SimpleExoPlayer player;
+    String videoURl = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4";
+
+    private Boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playPackPosition = 0;
+
+    Button btnVideo_2;
+    Button goToActivity3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        playerView = findViewById(R.id.video_view);
+        btnVideo_2 = findViewById(R.id.Btn_Video2);
+        goToActivity3 = findViewById(R.id.goToActivity3);
+
+        btnVideo_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initVideo();
+            }
+        });
+
+        goToActivity3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity3.class);
+                startActivity(i);
+                releaseVideo();
+            }
+        });
+    }
+
+    public void initVideo() {
+        //create player
+        player = ExoPlayerFactory.newSimpleInstance(this);
+        //connect Player with playerView
+        playerView.setPlayer(player);
+        //media source
+        Uri uri = Uri.parse(videoURl);
+        DataSource.Factory factory = new DefaultDataSourceFactory(this, "exoplayer-codelab");
+        MediaSource mediaSource = new ProgressiveMediaSource.Factory(factory).createMediaSource(uri);
+
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playPackPosition);
+        player.prepare(mediaSource, false, false);
+    }
+
+
+    public void releaseVideo() {
+        if (player != null) {
+            playWhenReady = player.getPlayWhenReady();
+            playPackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            player.release();
+            player = null;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (player != null) {
+            initVideo();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseVideo();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseVideo();
+    }
+}
